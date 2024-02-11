389dfdad2f7852224432106c45a4923e6485060e
Pablo Borsone <pabloborsone@hotmail.com>
2024-02-10 21:25:56 -0300

    Initial Commit

---
+++ RecipesTracker/RecipesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesTrackerApp.swift
+//  RecipesTracker
+//
+//  Created by Pablo Borsone on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
