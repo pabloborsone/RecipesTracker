23c1d26e3e86571f2838598d1f52f6abd9139d63
Pablo Borsone <pabloborsone@hotmail.com>
2024-02-29 13:46:44 -0300

    Create DetailsView as a placeholder for navigation purposes

---
+++ RecipesTracker/DetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsView.swift
+//  RecipesTracker
+//
+//  Created by Pablo Borsone on 28/02/24.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Image("card-image")
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    DetailsView()
+}
